d41397583e3b77dae3bca327903765d876af2748
Eduardo Pereira <ant.eduardo081@gmail.com>
2024-10-29 20:31:46 -0300

    add array manipulatio examples

--- 2-javascript/class-03-functions/script.js
+++ 2-javascript/class-03-functions/script.js
@@ -1,7 +1,68 @@
-function addNumbers ( num1, num2 ){
-    return num1 + num2;
-}
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
 
-console.log(addNumbers(255, 376));
-console.log(addNumbers(23, 56));
-console.log(addNumbers(255, 376));
+// function statements
+
+// named
+function printText(text /*parameters*/) {
+    console.log(text);
+  }
+  
+  printText("Text"); //arguments
+  printText(123);
+  
+  function sumNumbers(num1 = 0, num2 = 0) {
+    const sum = num1 + num2; // key operator
+    return sum;
+  }
+  
+  console.log(sumNumbers(20, 3));
+  
+  const resultSum = sumNumbers(2, 30);
+  console.log(resultSum);
+  
+  // anonymous
+  const sumNumbers = function (num1, num2) {
+    return num1 + num2;
+  };
+  
+  // arrow *
+  const sumNumbers = (num1, num2) => num1 + num2;
+  
+  console.log(sumNumbers(2, 5));
+  console.log(sumNumbers);
+  
+  // callback
+  function show(item) {
+    console.log(item);
+  }
+  
+  function sumNumbersShow(num1, num2, callback) {
+    let sum = num1 + num2;
+    callback(sum);
+  }
+  
+  sumNumbersShow(2, 3, show);
+  
+  // factory (creational design pattern)
+  
+  function person(name, age) {
+    return {
+      name,
+      age,
+      canDrive: age >= 18,
+      status: () => {
+        if (age >= 18) {
+          return "Pode dirigir";
+        } else {
+          return "Não pode dirigir";
+        }
+      },
+    };
+  }
+  
+  console.log(person("Emanuel", 30));
+  console.log(typeof person("Emanuel", 30));
+  console.log(person("Emanuel", 30).status());
+  
+  const user = person("Emanuel", 30);
+  console.log(user.status());
--- 2-javascript/class-04-data-manipulation/script.js
+++ 2-javascript/class-04-data-manipulation/script.js
@@ -13,7 +13,7 @@ console.log(myName.slice(-2));
 console.log(myName.slice(2, -3));
 
 console.log(concept.length);
-console.log(concept.indexOf("("));//contar as palavras por arrey//
+console.log(concept.indexOf("("));//contar as palavras por array//
 
 // Numbers
 
@@ -29,7 +29,7 @@ console.log(Math.pow(2,10)); // potencia
 
 console.log(Math.abs(num2));
 console.log(Math.trunc(num2));// Sem os numeos quebrados
-
+// Aredondamento
 console.log(Math.round(5.4));//0 ao 4 arredonda para baixo /5 ao 9 para cima
 console.log(Math.ceil(5.001));// aredonda sepre para cima
 console.log(Math.floor(5.999));// arredonda sempre para baixo
@@ -41,3 +41,38 @@ const times = ["corinthians","Flamengo","Vasco","Fortaleza","Ceará","São paulo
 
 const randomNumber = Math.floor(Math.random() * times.length);
 console.log(times[randomNumber]);
+
+// Array
+
+const crush = ["Tiazinha","Maria juaquina","Rambo","Bolzo","Paola Oliveira","Mariana Rui Barbosa"];
+console.log(crush.length);
+console.log(crush[1]);
+crush[0]="Feticeira";
+console.log(crush);
+
+let a = "Eduardo"
+a[1] = "E" // string is immutable!
+console.log(a);
+
+console.log(crush.join("-"));
+console.log("Bem á vindo".replaceAll(" ", "-"));
+
+// pergunta se algo esta presente o array
+console.log(crush.includes("Eduard"));
+console.log(crush.includes("Rambo"));
+
+console.log("Antonio Eduardo Pereira Oliveira".split(" ") [1]);
+
+// inverter a string
+ console.log(crush.reverse());
+console.log("Eduardo".split("").reverse().join(""));
+
+// Crie uma função que receba um texto como parãmetro e diga quantas palavras ele tem.
+
+function wordCount(text) {
+    //cauculo
+    const count = text.split(" ").length;
+    return count;
+}
+const text = "Antonio Eduardo pereira Oliveira"
+console.log(wordCount(text));
