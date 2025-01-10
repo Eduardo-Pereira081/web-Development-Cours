f4f820213e83b648488990923a23a92ce5ea4beb
Eduardo pereira <ant.eduardo081@gmail.com>
2025-01-10 20:17:40 -0300

    add user effect

--- 6-React/class-03-hooks/src/App.tsx
+++ 6-React/class-03-hooks/src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useState, userEffect } from "react";
 
 export function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
-  const [users, setUsers] = useState(["user1"," user2","user3"]);
+  const [users, setUsers] = useState(["user1", " user2"]);
 
   function add() {
     setCount(count + 1);
   }
+
+  function handleAddUser() {
+    const number = users.length + 1;
+    setUsers([...users, "user" + number]);
+  }
+
+  function handleRemoveUser(name: string) {
+    const filterArray = users.filter((user) => user != name);
+    setUsers(filterArray);
+  }
+
   function handleName(event: React.ChangeEvent<HTMLInputElement>) {
     setName(event.target.value);
   }
+  //alwys run when the component is rendered
+  userEffect(() => {
+    console.log("Renderizou");
+  }, [count, name]); // dependecy array
+
+  console.log("AAAA");
 
   return (
     <>
@@ -23,8 +40,16 @@ export function App() {
       <p>Name: {name}</p>
       <input type=" text" onInput={handleName} />
 
+      <h2>Users</h2>
+
+      <button onClick={handleAddUser}>Add user</button>
+
       {users.map((user) => {
-        return <p key={user}>{user}</p>;
+        return (
+          <p key={user}>
+            {user} <button onClick={() => handleRemoveUser(user)}>X</button>
+          </p>
+        );
       })}
     </>
   );
