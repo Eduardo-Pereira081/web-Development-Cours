3d4b366e1cb1d638f4128bef1bbda9af643beca4
Eduardo pereira <ant.eduardo081@gmail.com>
2025-01-15 20:21:26 -0300

    add class-04-context

--- 6-React/class-03-hooks/src/App.tsx
+++ 6-React/class-03-hooks/src/App.tsx
@@ -1,56 +1,15 @@
-import { useState, userEffect } from "react";
+import { AddButton } from "./components/AddButton";
+import { CountCard } from "./components/CountCard";
+import { ValueProvider } from "./contexts/valueContext";
 
 export function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-  const [users, setUsers] = useState(["user1", " user2"]);
-
-  function add() {
-    setCount(count + 1);
-  }
-
-  function handleAddUser() {
-    const number = users.length + 1;
-    setUsers([...users, "user" + number]);
-  }
-
-  function handleRemoveUser(name: string) {
-    const filterArray = users.filter((user) => user != name);
-    setUsers(filterArray);
-  }
-
-  function handleName(event: React.ChangeEvent<HTMLInputElement>) {
-    setName(event.target.value);
-  }
-  //alwys run when the component is rendered
-  userEffect(() => {
-    console.log("Renderizou");
-  }, [count, name]); // dependecy array
-
-  console.log("AAAA");
-
   return (
-    <>
-      <h1>Class 03 Hooks</h1>
+    <ValueProvider>
+      <h1>Class 04 Contexts</h1>
 
-      <p>count: {count}</p>
+      <AddButton />
 
-      <button onClick={add}>add</button>
-
-      <p>Name: {name}</p>
-      <input type=" text" onInput={handleName} />
-
-      <h2>Users</h2>
-
-      <button onClick={handleAddUser}>Add user</button>
-
-      {users.map((user) => {
-        return (
-          <p key={user}>
-            {user} <button onClick={() => handleRemoveUser(user)}>X</button>
-          </p>
-        );
-      })}
-    </>
+      <CountCard />
+    </ValueProvider>
   );
 }
